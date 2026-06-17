0fd4af4a3fc598e41b85f27a6911f17b15e75211
Dovran <dovran.tech@gmail.com>
2026-06-17 20:02:19 +0500

    Implement password hashing using BCrypt

---
+++ src/DilcomEducationCenter.Infrastructure/Security/PasswordHasher/BCryptPasswordHasher.cs
@@ -0,0 +1,16 @@
+using DilcomEducationCenter.Application.Abstractions;
+
+namespace DilcomEducationCenter.Infrastructure.Security.PasswordHasher;
+
+internal sealed class BCryptPasswordHasher : IPasswordHasher
+{
+    public string Hash(string password)
+    {
+        return BCrypt.Net.BCrypt.HashPassword(password);
+    }
+
+    public bool Verify(string password, string passwordHash)
+    {
+        return BCrypt.Net.BCrypt.Verify(password, passwordHash);
+    }
+}
